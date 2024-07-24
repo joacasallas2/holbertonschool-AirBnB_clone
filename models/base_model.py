1912df5d29bb7d2a4db0e283e1a390980f9f5ad5
joacasallas2 <proyecta.ds@gmail.com>
2024-07-24 16:05:22 -0500

    5. Store first object

--- models/base_model.py
+++ models/base_model.py
@@ -3,29 +3,32 @@
 """This module provides a class BaseModel"""
 import uuid
 import datetime
+import models
 
 
 class BaseModel:
     "Defines all common attributes/methods for other classes"
     def __init__(self, *args, **kwargs):
-        """Initialize the data"""
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.datetime.now()
-        self.updated_at = datetime.datetime.now()
-
-        if len(kwargs) > 0:
+        """Initializes a new instance of BaseModel"""
+        if kwargs:
             for key, value in kwargs.items():
                 if key == "__class__":
                     continue
                 self.__dict__[key] = value
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.datetime.now()
+            self.updated_at = datetime.datetime.now()
+            models.storage.new(self)
 
     def __str__(self):
         """prints in stdout: [<class name>] (<self.id>) <self.__dict__>"""
-        return f"[{__class__.__name__}] ({self.id}) {self.__dict__}"
+        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
 
     def save(self):
         """updates the attribute updated_at with the current datetime"""
         self.updated_at = datetime.datetime.now()
+        models.storage.save()
 
     def to_dict(self):
         """returns a dictionary containing all keys/values of __dict__"""
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,3 +1,48 @@
 #!/usr/bin/python3
 # Author: Joana Casallas
-"""Storage"""
+"""This module provides a class FileStorage"""
+import json
+import os
+from models.base_model import BaseModel
+from datetime import datetime
+
+
+class FileStorage:
+    """Serializes instances to and from JSON file"""
+    __file_path = "file.json"
+    __objects = {}
+
+    def __init__(self):
+        """Initializes the data"""
+        self.reload()
+
+    def all(self):
+        """returns the dictionary __objects"""
+        return self.__class__.__objects
+
+    def new(self, obj):
+        """sets in __objects the obj with key <obj class name>.id"""
+        key = "{}.{}".format(type(obj).__name__, obj.id)
+        self.__class__.__objects[key] = obj
+
+    def save(self):
+        """serializes __objects to the JSON file (path: __file_path)"""
+        filename = self.__file_path
+        with open(filename, "w", encoding="utf-8") as f:
+            data = {}
+            for k, v in self.__class__.__objects.items():
+                data[k] = v.to_dict()
+            json.dump(data, f)
+
+    def reload(self):
+        """deserializes the JSON file to __objects"""
+        if os.path.exists(self.__file_path):
+            with open(self.__file_path, "r", encoding="utf-8") as f:
+                dict_objs = json.load(f)
+                for key, value in dict_objs.items():
+                    value["created_at"] = datetime.fromisoformat(
+                        value["created_at"])
+                    value["updated_at"] = datetime.fromisoformat(
+                        value["updated_at"])
+                    obj = eval(value["__class__"])(**value)
+                    self.__objects[key] = obj
