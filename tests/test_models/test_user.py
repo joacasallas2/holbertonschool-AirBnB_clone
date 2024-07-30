532a3f0fab04eef55a5f8658a3832b77dc421f59
joacasallas2 <proyecta.ds@gmail.com>
2024-07-30 13:35:10 -0500

    tests class User

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -31,6 +31,7 @@ class TestUser(unittest.TestCase):
         """Test of User class for assigning or updating the user email"""
         self.assertIn("email", self.my_user_dict)
         self.assertEqual(self.my_user_dict["email"], "airbnb@mail.com")
+        self.assertTrue('email' in self.my_user.__dict__)
 
     def test_user_first_name(self):
         """Test of User class for assigning or updating the first name"""
@@ -46,6 +47,17 @@ class TestUser(unittest.TestCase):
         """Test of User class for assigning or updating the password"""
         self.assertIn("password", self.my_user_dict)
         self.assertEqual(self.my_user_dict["password"], "root")
+
+    def test_attributes_User(self):
+        """chekcing if User have attributes"""
+        self.assertTrue('email' in self.my_user.__dict__)
+        self.assertTrue('id' in self.my_user.__dict__)
+        self.assertTrue('created_at' in self.my_user.__dict__)
+        self.assertTrue('updated_at' in self.my_user.__dict__)
+        self.assertTrue('password' in self.my_user.__dict__)
+        self.assertTrue('first_name' in self.my_user.__dict__)
+        self.assertTrue('last_name' in self.my_user.__dict__)
+
 
 
 if __name__ == "__main__":
