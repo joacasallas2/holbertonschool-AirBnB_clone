80acd9e3a022dbf862afcb86673725b64fcb9f27
joacasallas2 <proyecta.ds@gmail.com>
2024-08-01 09:33:02 -0500

    tests user Class

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -1,35 +1,31 @@
-#!/usr/bin/python3
-"""test for user"""
+"""Tests User class"""
+
 import unittest
 import os
-from models.user import User
 from models.base_model import BaseModel
+from models.user import User
 import pep8
 
 
 class TestUser(unittest.TestCase):
-    """this will test the User class"""
+    """Group of test of User class"""
 
     @classmethod
     def setUpClass(cls):
-        """set up for test"""
+        """Create a new instance for each test"""
         cls.user = User()
-        cls.user.first_name = "Kevin"
-        cls.user.last_name = "Yook"
-        cls.user.email = "yook00627@gmamil.com"
-        cls.user.password = "secret"
+        cls.user.email = "airbnb@mail.com"
+        cls.user.first_name = "Betty"
+        cls.user.last_name = "Bar"
+        cls.user.password = "root"
+        cls.user.save()
+        cls.user_dict = cls.user.to_dict()
 
     @classmethod
-    def teardown(cls):
-        """at the end of the test this will tear it down"""
-        del cls.user
-
-    def tearDown(self):
+    def tearDownClass(cls):
         """teardown"""
-        try:
-            os.remove("file.json")
-        except Exception:
-            pass
+        os.remove("file.json")
+        del cls.user
 
     def test_pep8_User(self):
         """Tests pep8 style"""
@@ -37,11 +33,27 @@ class TestUser(unittest.TestCase):
         p = style.check_files(['models/user.py'])
         self.assertEqual(p.total_errors, 0, "fix pep8")
 
-    def test_checking_for_docstring_User(self):
-        """checking for docstrings"""
-        self.assertIsNotNone(User.__doc__)
+    def test_user_email(self):
+        """Test of User class for assigning or updating the user email"""
+        self.assertIn("email", self.user_dict)
+        self.assertEqual(self.user_dict["email"], "airbnb@mail.com")
 
-    def test_attributes_User(self):
+    def test_user_first_name(self):
+        """Test of User class for assigning or updating the first name"""
+        self.assertIn("first_name", self.user_dict)
+        self.assertEqual(self.user_dict["first_name"], "Betty")
+
+    def test_user_last_name(self):
+        """Test of User class for assigning or updating the last name"""
+        self.assertIn("last_name", self.user_dict)
+        self.assertEqual(self.user_dict["last_name"], "Bar")
+
+    def test_user_password(self):
+        """Test of User class for assigning or updating the password"""
+        self.assertIn("password", self.user_dict)
+        self.assertEqual(self.user_dict["password"], "root")
+
+    def test_attributes_user(self):
         """chekcing if User have attributes"""
         self.assertTrue('email' in self.user.__dict__)
         self.assertTrue('id' in self.user.__dict__)
@@ -51,25 +63,22 @@ class TestUser(unittest.TestCase):
         self.assertTrue('first_name' in self.user.__dict__)
         self.assertTrue('last_name' in self.user.__dict__)
 
-    def test_is_subclass_User(self):
-        """test if User is subclass of Basemodel"""
-        self.assertTrue(issubclass(self.user.__class__, BaseModel), True)
-
-    def test_attribute_types_User(self):
+    def test_attribute_types_user(self):
         """test attribute type for User"""
         self.assertEqual(type(self.user.email), str)
         self.assertEqual(type(self.user.password), str)
         self.assertEqual(type(self.user.first_name), str)
         self.assertEqual(type(self.user.first_name), str)
 
-    def test_save_User(self):
-        """test if the save works"""
-        self.user.save()
+    def test_is_subclass_user(self):
+        """tests if User is a subclass of BaseModel"""
+        self.assertTrue(issubclass(self.user.__class__, BaseModel), True)
+
+    def test_save_user(self):
+        """test the save method"""
         self.assertNotEqual(self.user.created_at, self.user.updated_at)
 
-    def test_to_dict_User(self):
-        """test if dictionary works"""
-        self.assertEqual('to_dict' in dir(self.user), True)
+    
 
 
 if __name__ == "__main__":
