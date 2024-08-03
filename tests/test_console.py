8c0d806355d2efc5ebb0ac616b712147909bd3f5
joacasallas2 <proyecta.ds@gmail.com>
2024-08-03 17:52:29 -0500

    tests console: quit command

--- tests/test_console.py
+++ tests/test_console.py
@@ -3,6 +3,7 @@
 import unittest
 import os
 from io import StringIO
+import pep8
 from unittest.mock import patch
 from models import storage
 from console import HBNBCommand
@@ -21,6 +22,12 @@ class TestHBNBCommand(unittest.TestCase):
         storage._FileStorage__objects = {}
         if os.path.exists("file.json"):
             os.remove("file.json")
+
+    def test_pep8_city(self):
+        """Tests pep8 style"""
+        style = pep8.StyleGuide(quiet=True)
+        p = style.check_files(['models/city.py'])
+        self.assertEqual(p.total_errors, 0, "fix pep8")
 
     def test_quit(self):
         """Test of HBNBCommand class for the quit command"""
